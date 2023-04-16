e2b8f196f8c2c94de080a92f501f83d62b6c4800
Kevin Narain <34915099+kimono-k@users.noreply.github.com>
2023-04-16 13:46:08 +0200

    Displaying critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -13,6 +13,7 @@ export interface Game {
     name: string;
     background_image: string;
     parent_platforms: { platform: Platform }[] // obj of arrays
+    metacritic: number;
 }
 
 interface FetchGamesResponse {
