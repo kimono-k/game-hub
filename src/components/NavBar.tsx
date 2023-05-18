c34d73344a999bef1c156298308ed3c0688c17e5
Kevin Narain <34915099+kimono-k@users.noreply.github.com>
2023-05-18 12:17:29 +0200

    Build search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,15 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
     // HStack is awesome for navbars, justify-content push it to the right
     // padding gives space when something pushed too far to the right
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
